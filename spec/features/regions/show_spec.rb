566e3bd4231a81241f9ba355b6d776405ad05a0d
Kevin Ta <kvnta@yahoo.com>
2022-08-25 12:19:54 -0600

    Finish user story 2

---
+++ spec/features/regions/show_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+RSpec.describe 'Region Show page' do 
+  it 'shows a specific parent attributes' do 
+    johto = Region.create!(name: "Johto", total_num_in_region: 151, caught_all_in_region: false)
+    sinoh = Region.create!(name: "Sinnoh", total_num_in_region: 301, caught_all_in_region: true)
+
+    visit "/regions/#{johto.id}"
+
+    expect(page).to have_content(johto.name)
+    expect(page).to have_content(johto.caught_all_in_region)
+    expect(page).to have_content(johto.total_num_in_region)
+    expect(page).to_not have_content(sinoh.name)
+  end
+end
