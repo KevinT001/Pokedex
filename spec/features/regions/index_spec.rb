fb06c0eb07d3aa66d71947508f5f72f621729e10
Kevin Ta <kvnta@yahoo.com>
2022-08-25 12:27:26 -0600

    remove save and open

--- spec/features/regions/index_spec.rb
+++ spec/features/regions/index_spec.rb
@@ -6,11 +6,12 @@ RSpec.describe 'Region index page' do
     sinoh = Region.create!(name: "Sinnoh", total_num_in_region: 301, caught_all_in_region: true )
 
     visit "/regions"
-    save_and_open_page
+    
     expect(page).to have_content(johto.name)
     expect(page).to have_content(sinoh.name)
     expect(page).to have_content(johto.total_num_in_region)
     expect(page).to have_content(sinoh.total_num_in_region)
-    
   end
+
+  
 end
